49bd5fb46fce9e2ed259537e3c188be185e93db0
lukaszjargielo <l.jargielo88@gmail.com>
2022-10-26 19:00:43 +0200

    New program - boolean and char type

---
+++ src/com/example/basics/data/types/J04_booleanChar.java
@@ -0,0 +1,17 @@
+package com.example.basics.data.types;
+
+public class J04_booleanChar {
+    public static void main(String[] args) {
+        boolean flag1 = true;
+        boolean flag2 = false;
+        System.out.println("flag1 ma warto¶æ " + flag1);
+        System.out.println("flag2 ma warto¶æ " + flag2);
+
+
+        char sign1 = '\u006A';
+        System.out.println("znak Unicode o numerze 006A w notacji hex to: " + sign1);
+
+
+
+    }
+}
