1f6dd685c75da5213d2bcceb81cd09c29f6cc9c3
lukaszjargielo <l.jargielo88@gmail.com>
2022-10-26 14:59:15 +0200

    New program - declaration of variable

---
+++ src/com/example/basics/data/types/J01_variable_declaration.java
@@ -0,0 +1,12 @@
+package com.example.basics.data.types;
+
+public class J01_variable_declaration {
+    public static void main(String[] args) {
+        int number =24;
+        System.out.println(number);
+        System.out.println("Warto¶æ zmiennej number " + number);
+        number = 25;
+        System.out.println("Warto¶æ zmiennej number po zmianie jej warto¶ci "+ number);
+        
+    }
+}
