d13b9985d3352f10b744e9bc1d84e38d994a672c
lukaszjargielo <l.jargielo88@gmail.com>
2022-10-27 10:36:59 +0200

    New program - different variables

---
+++ src/com/example/basics/data/types/J05_variableNames.java
@@ -0,0 +1,11 @@
+package com.example.basics.data.types;
+
+public class J05_variableNames {
+    public static void main(String[] args) {
+         int _count = 0;
+         int currentHour = 12;
+         float $moneyInThePocket = 23.12f;
+         int CurrentHour = 24;
+
+    }
+}
