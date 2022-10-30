3a85bfb61eb4bd5e795509e633da318cf5863f27
lukaszjargielo <l.jargielo88@gmail.com>
2022-10-30 17:26:49 +0100

    Parallel project about variables

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        byte passengersLimit = 5;
+        short totalVehicleWeightInKilograms = 2_787;
+        int totalMilageInKilometers = 323_865;
+        float averangeFuelConsumptionInLitersPer100Km = 8.7f;
+        boolean isImported = false;
+    }
+}
