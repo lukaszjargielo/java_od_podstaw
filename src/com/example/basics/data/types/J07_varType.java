ff61b8d7acac2645260bbce7dd3e5afada55e54d
lukaszjargielo <l.jargielo88@gmail.com>
2022-10-31 12:14:46 +0100

    Type var

---
+++ src/com/example/basics/data/types/J07_varType.java
@@ -0,0 +1,14 @@
+package com.example.basics.data.types;
+
+public class J07_varType {
+    public static void main(String[] args) {
+        int number = add(10,"5");
+        System.out.println(number);
+    }
+
+    public static int add(int a, String b) {
+        var result = a + Integer.valueOf(b).intValue();
+        return result;
+
+    }
+}
