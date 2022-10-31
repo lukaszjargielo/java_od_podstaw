1268b78dff6dca404d3f58383723e8239789f0f8
lukaszjargielo <l.jargielo88@gmail.com>
2022-10-31 12:58:24 +0100

    Keyword final

---
+++ src/com/example/basics/data/types/J08_finalKeyword.java
@@ -0,0 +1,11 @@
+package com.example.basics.data.types;
+
+public class J08_finalKeyword {
+    public static void main(String[] args) {
+        int num1 = 10;
+        num1 = 32;
+
+        final int num2 = 100;
+        //num2 = 130;
+    }
+}
