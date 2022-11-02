0435beefbfabc8a069c59eaeedef7024e38ebf9b
lukaszjargielo <l.jargielo88@gmail.com>
2022-11-02 19:38:36 +0100

    Methods of Math class

---
+++ src/com/example/basics/data/MathExample.java
@@ -0,0 +1,14 @@
+package com.example.basics.data;
+import java.lang.*;
+
+public class MathExample {
+    public static void main(String[] args) {
+
+        double d = 16.0;
+        double sqrt = Math.sqrt(d);
+        System.out.println("The square root of 16 is: " + sqrt);
+
+        double power = Math.pow(d, 2);
+        System.out.println("d squared is: " + power);
+    }
+}
