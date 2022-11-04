4f53504c7fe8635d7061d3c4e6a43c9db10e4790
lukaszjargielo <l.jargielo88@gmail.com>
2022-11-04 13:08:54 +0100

    Operators - basics

---
+++ src/com/example/basics/operators/Op1_basics.java
@@ -0,0 +1,17 @@
+package com.example.basics.operators;
+
+public class Op1_basics {
+    public static void main(String[] args) {
+        int a = 10;
+        int b, c;
+
+        c = b = a +5;
+        System.out.println("a:" + a + " b:" + b + " c:" + c);
+
+        int v = (100 *2) / 5 + 20;
+        System.out.println("v:" + v);
+
+        System.out.println("20 % 7: " + 20%7);
+
+    }
+}
