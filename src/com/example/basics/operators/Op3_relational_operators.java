3ebd714b23286987573934672bbb5aa5ed7ba08a
lukaszjargielo <l.jargielo88@gmail.com>
2022-11-04 15:43:54 +0100

    Relational operators - examples

---
+++ src/com/example/basics/operators/Op3_relational_operators.java
@@ -0,0 +1,30 @@
+package com.example.basics.operators;
+
+public class Op3_relational_operators {
+    public static void main(String[] args) {
+        boolean b1 = 5 == 3;
+        System.out.println(b1);
+        boolean b2 = 7 == 7;
+        System.out.println(b2);
+        boolean b3 = 7 != 7;
+        System.out.println(b3);
+        boolean b4 = 7 != 6;
+        System.out.println(b4);
+        boolean b5 = 10 > 11;
+        System.out.println(b5);
+        boolean b6 = 10 > 9;
+        System.out.println(b6);
+        boolean b7 = 10 < 6;
+        System.out.println(b7);
+        boolean b8 = 10 < 11;
+        System.out.println(b8);
+        boolean b9 = 10 >= 11;
+        System.out.println(b9);
+        boolean b10 = 10 >= 10;
+        System.out.println(b10);
+        boolean b11 = 10 <= 9;
+        System.out.println(b11);
+        boolean b12 = 10 <= 10;
+        System.out.println(b12);
+    }
+}
