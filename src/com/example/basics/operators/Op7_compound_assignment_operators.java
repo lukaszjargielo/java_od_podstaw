43f4907ae4aff823f6d5da1afe6bd53723894cbb
lukaszjargielo <l.jargielo88@gmail.com>
2022-11-04 19:10:59 +0100

    Some examples of use compound assignment operators

---
+++ src/com/example/basics/operators/Op7_compound_assignment_operators.java
@@ -0,0 +1,16 @@
+package com.example.basics.operators;
+
+public class Op7_compound_assignment_operators {
+    public static void main(String[] args) {
+
+        int a = 5;
+        a += 10;
+        System.out.println(a);
+        a += a;
+        System.out.println(a);
+        a *= a;
+        System.out.println(a);
+        a %= 7;
+        System.out.println(a + " because 7 times 128 plus 4 equals 900  ");
+    }
+}
