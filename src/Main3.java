83d4f97c531b8c74465d69474c6160a865922625
lukaszjargielo <l.jargielo88@gmail.com>
2022-11-05 17:06:58 +0100

    Some stage with more complex code

---
+++ src/Main3.java
@@ -0,0 +1,21 @@
+public class Main3 {
+    public static void main(String[] args) {
+
+
+
+        boolean raining = false;
+        boolean haveUmbrella = false;
+        int temperature = 20;
+
+        if((temperature > -20)&&(temperature < 40)) {
+                else if ( ((temperature > -10) && (temperature < 10)) && ((raining) && (haveUmbrella)) ) {
+                System.out.println("Temperature is between -10 and 10 Celcius degrees. It' rainy, but you have an umbrella. You can go for a walk, stupid!");
+            } else {
+                System.out.println("");
+            }
+        } else {
+            System.out.println("There is apocalypse outside. Stay at atomic shelter!");
+
+    }
+    }
+}
