8510c66be1528030f03ff6c510745df92ec4d883
lukaszjargielo <l.jargielo88@gmail.com>
2022-11-05 13:18:23 +0100

    Program checking if numer is positive/negative/equals zero

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) {
+
+        {
+            int number = 15;
+
+            if (number < 0) {
+                System.out.println("Given number(" + number +  ") is less than zero");
+            } else if (number == 0) {
+                System.out.println("Given number(" + number +  ") equals zero");
+            } else {
+                System.out.println("Given number(" + number +  ") is greater than zero");
+            }
+        }
+
+        {
+            int number = -8;
+
+            if (number < 0) {
+                System.out.println("Given number(" + number +  ") is less than zero");
+            } else if (number == 0) {
+                System.out.println("Given number(" + number +  ") equals zero");
+            } else {
+                System.out.println("Given number(" + number +  ") is greater than zero");
+            }
+        }
+
+        {
+            int number = 0;
+
+            if (number < 0) {
+                System.out.println("Given number(" + number +  ") is less than zero");
+            } else if (number == 0) {
+                System.out.println("Given number(" + number +  ") equals zero");
+            } else {
+                System.out.println("Given number(" + number +  ") is greater than zero");
+            }
+        }
+    }
+}
