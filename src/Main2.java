b463557c00014f27341a708eaa26bb6fb84a6549
lukaszjargielo <l.jargielo88@gmail.com>
2022-11-05 13:43:51 +0100

    Program about blood donors and vampires >:>

---
+++ src/Main2.java
@@ -0,0 +1,79 @@
+public class Main2 {
+    public static void main(String[] args) {
+
+        {
+            int age = 18;
+            int weight = 50;
+            System.out.println("age[years]/weight[kg]: " + age + "/" + weight);
+
+            if (age >= 18) {
+                if (weight >= 50) {
+                    System.out.println("You can be blood donor. Your vampire is waiting for U!");
+                } else {
+                    System.out.println("You should gain weight! Vampires hate skinny people!");
+                }
+            } else if (weight >= 50) {
+                System.out.println("Your weight is OK but you are underage! If you want to play with vampire you must be of age!");
+            } else {
+                System.out.println("Both weight and age are inadequate! Go back to mummy!");
+
+            }        }
+
+        {
+            int age = 18;
+            int weight = 49;
+            System.out.println("age[years]/weight[kg]: " + age + "/" + weight);
+
+            if (age >= 18) {
+                if (weight >= 50) {
+                    System.out.println("You can be blood donor. Your vampire is waiting for U!");
+                } else {
+                    System.out.println("You should gain weight! Vampires hate skinny people!");
+                }
+            } else if (weight >= 50) {
+                System.out.println("Your weight is OK but you are underage! If you want to play with vampire you must be of age!");
+            } else {
+                System.out.println("Both weight and age are inadequate! Go back to mummy!");
+
+            }
+        }
+
+        {
+            int age = 17;
+            int weight = 50;
+            System.out.println("age[years]/weight[kg]: " + age + "/" + weight);
+
+            if (age >= 18) {
+                if (weight >= 50) {
+                    System.out.println("You can be blood donor. Your vampire is waiting for U!");
+                } else {
+                    System.out.println("You should gain weight! Vampires hate skinny people!");
+                }
+            } else if (weight >= 50) {
+                System.out.println("Your weight is OK but you are underage! If you want to play with vampire you must be of age!");
+            } else {
+                System.out.println("Both weight and age are inadequate! Go back to mummy!");
+
+            }
+        }
+
+        {
+            int age = 17;
+            int weight = 49;
+            System.out.println("age[years]/weight[kg]: " + age + "/" + weight);
+
+            if (age >= 18) {
+                if (weight >= 50) {
+                    System.out.println("You can be blood donor. Your vampire is waiting for U!");
+                } else {
+                    System.out.println("You should gain weight! Vampires hate skinny people");
+                }
+            } else if (weight >= 50) {
+                System.out.println("Your weight is OK but you are underage! If you want to play with vampire you must be of age!");
+            } else {
+                System.out.println("Both weight and age are inadequate! Go back to mummy!");
+
+            }
+        }
+    }
+}
