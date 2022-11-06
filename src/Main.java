cf767b480fa390ba7b74a0eaf95f2067e6e5a96b
lukaszjargielo <l.jargielo88@gmail.com>
2022-11-06 18:47:03 +0100

    Program about seasons with switch statement

---
+++ src/Main.java
@@ -0,0 +1,171 @@
+public class Main {
+    public static void main(String[] args) {
+        {
+            int monthNum = 1;
+            String season = null;
+            System.out.println("Month number:" + monthNum);
+
+            switch (monthNum) {
+                case 3:
+                case 4:
+                case 5:
+                    season = "spring";
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    season = "summer";
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    season = "autumn";
+                    break;
+                case 12:
+                case 1:
+                case 2:
+                    season = "winter";
+                    break;
+                default:
+                    season = "wrong number";
+
+            }
+
+            System.out.println(season);
+        }
+
+        {
+            int monthNum = 3;
+            String season = null;
+            System.out.println("Month number:" + monthNum);
+
+            switch (monthNum) {
+                case 3:
+                case 4:
+                case 5:
+                    season = "spring";
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    season = "summer";
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    season = "autumn";
+                    break;
+                case 12:
+                case 1:
+                case 2:
+                    season = "winter";
+                    break;
+                default:
+                    season = "wrong number";
+
+            }
+
+            System.out.println(season);
+        }
+
+        {
+            int monthNum = 7;
+            String season = null;
+            System.out.println("Month number:" + monthNum);
+
+            switch (monthNum) {
+                case 3:
+                case 4:
+                case 5:
+                    season = "spring";
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    season = "summer";
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    season = "autumn";
+                    break;
+                case 12:
+                case 1:
+                case 2:
+                    season = "winter";
+                    break;
+                default:
+                    season = "wrong number";
+
+            }
+
+            System.out.println(season);
+        }
+
+        {
+            int monthNum = 10;
+            String season = null;
+            System.out.println("Month number:" + monthNum);
+
+            switch (monthNum) {
+                case 3:
+                case 4:
+                case 5:
+                    season = "spring";
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    season = "summer";
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    season = "autumn";
+                    break;
+                case 12:
+                case 1:
+                case 2:
+                    season = "winter";
+                    break;
+                default:
+                    season = "wrong number";
+            }
+
+            System.out.println(season);
+        }
+
+        {
+            int monthNum = 13;
+            String season = null;
+            System.out.println("Month number:" + monthNum);
+
+            switch (monthNum) {
+                case 3:
+                case 4:
+                case 5:
+                    season = "spring";
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    season = "summer";
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    season = "autumn";
+                    break;
+                case 12:
+                case 1:
+                case 2:
+                    season = "winter";
+                    break;
+                default:
+                    season = "wrong number";
+            }
+
+            System.out.println(season);
+        }
+    }
+}
