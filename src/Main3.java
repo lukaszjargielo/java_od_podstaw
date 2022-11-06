8e2994ed48110b395aa4bee10523edd4d4a4661f
lukaszjargielo <l.jargielo88@gmail.com>
2022-11-06 09:51:29 +0100

    Before changes in Main4

--- src/Main3.java
+++ src/Main3.java
@@ -2,20 +2,35 @@ public class Main3 {
     public static void main(String[] args) {
 
 
-
+        {
         boolean raining = false;
         boolean haveUmbrella = false;
         int temperature = 20;
 
+        System.out.println("Temperature:" + temperature + " raining:" + raining + " umbrella close at hand:" + haveUmbrella);
+
         if((temperature > -20)&&(temperature < 40)) {
-                else if ( ((temperature > -10) && (temperature < 10)) && ((raining) && (haveUmbrella)) ) {
-                System.out.println("Temperature is between -10 and 10 Celcius degrees. It' rainy, but you have an umbrella. You can go for a walk, stupid!");
-            } else {
-                System.out.println("");
+            if ((temperature > -10) && (temperature < 10)) {
+                if(raining) {
+                    if(haveUmbrella) {
+                        System.out.println("Temperature is between -10 and 10 Celcius degrees. It's rainy, but you have an umbrella. You can go for a walk, stupid!");
+                    } else {
+                        System.out.println("Temperature is between -10 and 10 Celcius degrees. It's rainy, but you don't have an umbrella. You can't go for a walk, stupid!");
+                    }
+                } else {
+                    System.out.println("Temperature is between -10 and 10 Celcius degrees. It's not rainy, so you don't have to worry about an umbrella. You can go for a walk, stupid!");
+                }
+            } else if (temperature >= 10) {
+                System.out.println("Temperature is above 10 Celcius degrees. It' rainy, but you have an umbrella. You can go for a walk, stupid!");
+
+
+//                    && ((raining) && (haveUmbrella)) ) {
+
             }
         } else {
             System.out.println("There is apocalypse outside. Stay at atomic shelter!");
 
-    }
+            }
+        }
     }
 }
--- src/Main4.java
+++ src/Main4.java
@@ -1,2 +1,125 @@
-package PACKAGE_NAME;public class Main4 {
+public class Main4 {
+
+    public static void main(String[] args) {
+        {
+            boolean raining = true;
+            boolean haveUmbrella = true;
+            int temperature = -21;
+
+            System.out.println("Temperature:" + temperature + " Raining:" + raining + " Umbrella close at hand:" + haveUmbrella);
+
+            if((temperature < -20) || (temperature > 40)) {
+                System.out.println("Stay at home");
+            } else {
+                if( ((temperature > -10)&&(temperature < 10)) && ((raining)&&(haveUmbrella)) ){
+                    System.out.println("The temperature is between -10 and 10 Celcius degrees, it's rainy but you have an umbrella. You can go for a walk.");
+                } else if ((temperature > 12)&&(!raining)) {
+                    System.out.println("It's not raining and temperature is above 12 Celcius degrees. You can go for a walk.");
+                } else {
+                    System.out.println("It's complicated! You would better stay home.");
+                }
+            }
+        }
+
+        {
+            boolean raining = true;
+            boolean haveUmbrella = true;
+            int temperature = 41;
+
+            System.out.println("Temperature:" + temperature + " Raining:" + raining + " Umbrella close at hand:" + haveUmbrella);
+
+            if((temperature < -20) || (temperature > 40)) {
+                System.out.println("Stay at home");
+            } else {
+                if( ((temperature > -10)&&(temperature < 10)) && ((raining)&&(haveUmbrella)) ){
+                    System.out.println("The temperature is between -10 and 10 Celcius degrees, it's rainy but you have an umbrella. You can go for a walk.");
+                } else if ((temperature > 12)&&(!raining)) {
+                    System.out.println("It's not raining and temperature is above 12 Celcius degrees. You can go for a walk.");
+                } else {
+                    System.out.println("It's complicated! You would better stay home.");
+                }
+            }
+        }
+
+        {
+            boolean raining = true;
+            boolean haveUmbrella = true;
+            int temperature = -8;
+
+            System.out.println("Temperature:" + temperature + " Raining:" + raining + " Umbrella close at hand:" + haveUmbrella);
+
+            if ((temperature < -20) || (temperature > 40)) {
+                System.out.println("Stay at home");
+            } else {
+                if (((temperature > -10) && (temperature < 10)) && ((raining) && (haveUmbrella))) {
+                    System.out.println("The temperature is between -10 and 10 Celcius degrees, it's rainy but you have an umbrella. You can go for a walk.");
+                } else if ((temperature > 12) && (!raining)) {
+                    System.out.println("It's not raining and temperature is above 12 Celcius degrees. You can go for a walk.");
+                } else {
+                    System.out.println("It's complicated! You would better stay home.");
+                }
+            }
+        }
+
+            {
+                boolean raining = true;
+                boolean haveUmbrella = false;
+                int temperature = -8;
+
+                System.out.println("Temperature:" + temperature + " Raining:" + raining + " Umbrella close at hand:" + haveUmbrella);
+
+                if((temperature < -20) || (temperature > 40)) {
+                    System.out.println("Stay at home");
+                } else {
+                    if( ((temperature > -10)&&(temperature < 10)) && ((raining)&&(haveUmbrella)) ){
+                        System.out.println("The temperature is between -10 and 10 Celcius degrees, it's rainy but you have an umbrella. You can go for a walk.");
+                    } else if ((temperature > 12)&&(!raining)) {
+                        System.out.println("It's not raining and temperature is above 12 Celcius degrees. You can go for a walk.");
+                    } else {
+                        System.out.println("It's complicated! You would better stay home.");
+                    }
+                }
+            }
+
+        {
+            boolean raining = false;
+            boolean haveUmbrella = true;
+            int temperature = -8;
+
+            System.out.println("Temperature:" + temperature + " Raining:" + raining + " Umbrella close at hand:" + haveUmbrella);
+
+            if((temperature < -20) || (temperature > 40)) {
+                System.out.println("Stay at home");
+            } else {
+                if( ((temperature > -10)&&(temperature < 10)) && ((raining)&&(haveUmbrella)) ){
+                    System.out.println("The temperature is between -10 and 10 Celcius degrees, it's rainy but you have an umbrella. You can go for a walk.");
+                } else if ((temperature > 12)&&(!raining)) {
+                    System.out.println("It's not raining and temperature is above 12 Celcius degrees. You can go for a walk.");
+                } else {
+                    System.out.println("It's complicated! You would better stay home.");
+                }
+            }
+        }
+
+        {
+            boolean raining = false;
+            boolean haveUmbrella = true;
+            int temperature = 13;
+
+            System.out.println("Temperature:" + temperature + " Raining:" + raining + " Umbrella close at hand:" + haveUmbrella);
+
+            if((temperature < -20) || (temperature > 40)) {
+                System.out.println("Stay at home");
+            } else {
+                if( ((temperature > -10)&&(temperature < 10)) && ((raining)&&(haveUmbrella)) ){
+                    System.out.println("The temperature is between -10 and 10 Celcius degrees, it's rainy but you have an umbrella. You can go for a walk.");
+                } else if ((temperature > 12)&&(!raining)) {
+                    System.out.println("It's not raining and temperature is above 12 Celcius degrees. You can go for a walk.");
+                } else {
+                    System.out.println("It's complicated! You would better stay home.");
+                }
+            }
+        }
+    }
+
 }
