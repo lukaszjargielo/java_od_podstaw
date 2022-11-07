ea760f2e23e29fd415c9b92ec66ec6666fc50cfb
lukaszjargielo <l.jargielo88@gmail.com>
2022-11-07 14:26:24 +0100

    Simple program about break

---
+++ src/Break.java
@@ -0,0 +1,9 @@
+public class Break {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 50; i++) {
+            System.out.println(i);
+            if (i == 10) break;
+        }
+    }
+}
