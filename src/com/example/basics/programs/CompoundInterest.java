a539533783d773e7db695d971f9c98f34e0b38eb
lukaszjargielo <l.jargielo88@gmail.com>
2022-11-07 19:34:54 +0100

    Program calculating profit from saving

---
+++ src/com/example/basics/programs/CompoundInterest.java
@@ -0,0 +1,29 @@
+package com.example.basics.programs;
+
+import java.util.Scanner;
+
+public class CompoundInterest {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double startCapital = 0;
+        double yearlyInterest = 0;
+        int numYears = 0;
+
+        double finalCapital;
+
+        System.out.println("Enter the amount of start capital in format [ 20 $ -> 20.00]: " );
+             startCapital =  Double.valueOf(scanner.nextLine()).doubleValue();
+        System.out.println("Enter yearly interest in format [5% -> 5]: ");
+            yearlyInterest = Double.valueOf(scanner.nextLine()).doubleValue();
+        System.out.println("Enter number of investment years ");
+            numYears = scanner.nextInt();
+
+            finalCapital = startCapital * Math.pow((1 + (yearlyInterest) / 100.0), numYears);
+
+        System.out.println("After " + numYears + " years of saving "
+                + startCapital + "$ with compound interest in the amount of "
+                + yearlyInterest + "% \nyou got " + Math.round(finalCapital) + "$" );
+    }
+}
