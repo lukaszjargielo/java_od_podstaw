14324c21b87b25e4bf422d0a28d6e2b952bdad3a
lukaszjargielo <l.jargielo88@gmail.com>
2022-11-08 11:34:56 +0100

    Examples of concatenation methods

---
+++ src/com/example/basics/data/string_class/ConcatenationExample.java
@@ -0,0 +1,21 @@
+package com.example.basics.data.string_class;
+
+public class ConcatenationExample {
+    public static void main(String[] args) {
+
+        String s1 = "1967";
+        String s2 = " Mustang ";
+        String s3 = s1 + s2 + " - the great car! ";
+        System.out.println(s3);
+
+        int i = 1967;
+        String s4 = i + s2;
+        System.out.println(s4);
+
+        String s5 = s1.concat(s2);
+        System.out.println(s5);
+
+        String s6 = s2.concat(Integer.toString(i));
+        System.out.println(s6);
+    }
+}
