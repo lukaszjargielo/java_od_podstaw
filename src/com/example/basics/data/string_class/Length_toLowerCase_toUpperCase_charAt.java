d34f7e08aca34955175f5acab85fd7ea33d4e2ca
lukaszjargielo <l.jargielo88@gmail.com>
2022-11-08 15:16:32 +0100

    Examples of methods: length() charAt() toLower/UpperCase

---
+++ src/com/example/basics/data/string_class/Length_toLowerCase_toUpperCase_charAt.java
@@ -0,0 +1,20 @@
+package com.example.basics.data.string_class;
+
+import java.util.Locale;
+
+public class Length_toLowerCase_toUpperCase_charAt {
+    public static void main(String[] args) {
+
+        String str = "You got it!";
+        System.out.println("Number of signs: " + str.length());
+
+        char c = str.charAt(str.length()-1);
+        System.out.println("Char at index 11: " + c);
+
+        String strUp = str.toUpperCase();
+        System.out.println(strUp);
+
+        String strDown = str.toLowerCase();
+        System.out.println(strDown);
+    }
+}
