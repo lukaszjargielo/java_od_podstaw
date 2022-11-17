0821c2ce99e4eef6f1c299b2c870f5af8a8d0717
lukaszjargielo <l.jargielo88@gmail.com>
2022-11-17 12:14:07 +0100

    Program with private access modifier

---
+++ src/com/example/oop/basics/access_modifiers/private_example/Car.java
@@ -0,0 +1,8 @@
+package com.example.oop.basics.access_modifiers.private_example;
+
+public class Car extends Product{
+    public Car() {
+//        this.name = "Car unknown";
+        super("unknown car");
+    }
+}
