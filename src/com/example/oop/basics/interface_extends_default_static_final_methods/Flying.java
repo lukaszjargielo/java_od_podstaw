24f6bc65b07a286d1979c12220e92733602711e6
lukaszjargielo <l.jargielo88@gmail.com>
2022-11-20 19:25:34 +0100

    Extending of interfaces

---
+++ src/com/example/oop/basics/interface_extends_default_static_final_methods/Flying.java
@@ -0,0 +1,16 @@
+package com.example.oop.basics.interface_extends_default_static_final_methods;
+
+public interface Flying {
+    public float DEFAULT_WINGSPAN = 0.7f;
+    public void fly();
+    public default void flyHigher() {
+        System.out.println("Flying higher");
+    }
+    public default float getWingspan() {
+        return Flying.DEFAULT_WINGSPAN;
+    }
+
+    public static int getDefaultNumWings(){
+        return 2;
+    }
+}
