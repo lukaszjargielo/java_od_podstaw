f21dce92707d7c02ffb0d912e3321e1f618c2bc3
lukaszjargielo <l.jargielo88@gmail.com>
2022-11-22 09:12:22 +0100

    Basics of exceptions

---
+++ src/com/example/oop/basics/exceptions/ExceptionsBasics.java
@@ -0,0 +1,65 @@
+package com.example.oop.basics.exceptions;
+
+public class ExceptionsBasics {
+    public static void main(String[] args) {
+
+        try {
+            int numArr[] = {0, 1, 2, 3, 4, 5};
+            int a = numArr[12];
+            int result = a / numArr[0];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Program works after exception");
+
+//        try {
+//            int numArr[] = {0, 1, 2, 3, 4, 5};
+//            int a = numArr[2];
+//            int result = a / numArr[0]; //here we have got Arithmetic Exception occuration
+//        } catch (ArrayIndexOutOfBoundsException e) { // here we catch another type of exception
+//            e.printStackTrace(); // so our programm will stop working
+//        }
+//        System.out.println("Program works after exception");
+        try {
+            int numArr[] = {0, 1, 2, 3, 4, 5};
+            int a = numArr[2];
+            int result = a / numArr[0];
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Program works after second exception");
+
+        try {
+            int numArr[] = {0, 1, 2, 3, 4, 5};
+            int a = numArr[2];
+            int result = a / numArr[0];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Program works after third exception");
+
+        try {
+            int numArr[] = {0, 1, 2, 3, 4, 5};
+            int a = numArr[12];
+            int result = a / numArr[0];
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("Program works after fourth exception");
+
+        try {
+            int numArr[] = {0, 1, 2, 3, 4, 5};
+            int a = numArr[12];
+            int result = a / numArr[0];
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("This finally clause will always be executed, " +
+                    "regardless of the exception");
+        }
+        System.out.println("Program works after fifth exception");
+    }
+
+}
